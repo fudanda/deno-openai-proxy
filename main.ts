225adaad137c548803dc7564193c27c7ba59f783
fudanda <314553951@qq.com>
2023-04-24 16:09:28 +0800

    🐛

--- main.ts
+++ main.ts
@@ -3,16 +3,13 @@ import { serve } from "https://deno.land/std@0.184.0/http/server.ts";
 const OPENAI_API_HOST = "api.openai.com";
 serve(async (request) => {
   const url = new URL(request.url);
-  console.log("Path:", url.pathname);
-  console.log("Query parameters:", url.searchParams);
-  console.log("key:", url.searchParams.get('key'));
-  console.log("url:", url);
+  const key=request.headers.get('Authorization');
   console.log("request:", request);
 
   if (url.pathname === "/") {
     return fetch(new URL("./README.md", import.meta.url));
   }
-  const key =('Bearer '+url.searchParams.get('key'))  as string;
+
   url.host = OPENAI_API_HOST;
   return await fetch(url, {
     headers: {
